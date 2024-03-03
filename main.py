d072e20a27384de5f80a4b9ec579f3cedfb21ab5
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-03-03 23:37:16 +0300

    Added Input Manipulation methods

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from pynput import keyboard, Key
+from pynput import keyboard
+import pynput
+import pyperclip
+
+controller = keyboard.Controller()
+
+def process_text(text):
+    return text[::-1]
+
 
 def selected_text():
-    print('ADDED F9 AS SHORTCUT')
+    with (controller.pressed(keyboard.Key.ctrl)):
+        controller.tap("c")
+    text = pyperclip.paste()
+    text = process_text(text)
+    pyperclip.copy(text)
+    
+    with (controller.pressed(keyboard.Key.ctrl)):
+        controller.tap("v")
+   
 
 def line_text():
-    print('ADDED F10 AS SHORTCUT')
+    controller.tap(keyboard.Key.home)
+    with (controller.pressed(keyboard.Key.shift)):
+        controller.tap(keyboard.Key.end)
+    
+    with (controller.pressed(keyboard.Key.ctrl)):
+        controller.tap("c")
+    
+    text = pyperclip.paste()
+    text = process_text(text)
+    
+    pyperclip.copy(text)
+    with (controller.pressed(keyboard.Key.ctrl)):
+        controller.tap("v")
 
-selected_shortcut = str(Key.f9.value) # F9
-line_shortcut = str(Key.f10.value) # F10
+selected_shortcut = str(keyboard.Key.f9.value) # F9
+line_shortcut = str(keyboard.Key.f10.value) # F10
 
 with keyboard.GlobalHotKeys({
         selected_shortcut: selected_text,
